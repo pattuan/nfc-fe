037d64e27fb9c25faa89c8c099cf00f3e6d41c1e
tuan <tonytuanpat2601@gmail.com>
2024-04-20 01:29:35 +0700

    update

--- src/page/logout/index.js
+++ src/page/logout/index.js
@@ -2,17 +2,14 @@ import { Link } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 
 const Logout = () => {
-  const [cookies, removeCookies] = useCookies(['token']);
+  const [cookies, setCookie, removeCookies] = useCookies(['token']);
   
   
   const handleLogout = () => {
     // Xóa cookie có tên 'token'
-    document.cookie.split(";").forEach((c) => {
-      document.cookie = c
-        .replace(/^ +/, "")
-        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-    });
-    // removeCookies('token');
+    
+    // document.cookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    removeCookies('token');
     window.location.reload();
     return false;
   };
